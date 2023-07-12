9d49c55b7a5841d7bf053c036b3a8d90e2380636
srgonroll <sarthak_660027@yahoo.co.in>
2023-07-13 01:15:20 +0530

    constructor OR simple initals

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,48 @@
 import React from "react";
 import  ReactDOM from "react-dom";
 
-class App extends React.Component{
+interface AppProps{
+    color:string
+
+}
+
+// (2) way in TS
+// interface AppState{
+//     counter:number
+// }
+
+
+class App extends React.Component<AppProps>{
+
+    state = {counter:0}                  //1st way to set state in TS
+
+    // constructor(props: AppProps){           // 2nd way to set inital state in TS(2)
+    //     super(props)
+    //     this.state = {counter: 0}
+    // }
+
+    // state = {counter:0}
+
+    onIncrement = ():void =>{
+        this.setState({counter: this.state.counter+1})
+    
+    }
+    
+    onDecrement = ():void =>{
+        this.setState({counter: this.state.counter - 1})
+    
+    }
+
     render(){
-        return <div>GG</div>
+        return <div>
+            <button onClick={this.onIncrement}>INCREMENT</button>
+            <button onClick={this.onDecrement}>DECREMENT</button>
+            {this.state.counter}
+        </div>
     }
 }
 
 ReactDOM.render(
-<App/>,
-document.querySelector('#root')
-)
+<App color= "red" />,
+document.querySelector('#root'))
 
