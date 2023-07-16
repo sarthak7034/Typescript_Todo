dd20bbf112f101a6d7139ee3d8ba9761b5ca94c3
srgonroll <sarthak_660027@yahoo.co.in>
2023-07-16 22:20:14 +0530

    redux setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,48 +1,59 @@
 import React from "react";
 import  ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { App } from "./components/App";
+import { reducers } from "./reducers";
 
-interface AppProps{
-    color:string
+//---------------------------------------------------------------------------------------------------------------
+// interface AppProps{
+//     color:string
 
-}
-
-// (2) way in TS
-// interface AppState{
-//     counter:number
 // }
 
+// // (2) way in TS
+// // interface AppState{
+// //     counter:number
+// // }
 
-class App extends React.Component<AppProps>{
 
-    state = {counter:0}                  //1st way to set state in TS
+// class App extends React.Component<AppProps>{
 
-    // constructor(props: AppProps){           // 2nd way to set inital state in TS(2)
-    //     super(props)
-    //     this.state = {counter: 0}
-    // }
+//     state = {counter:0}                  //1st way to set state in TS
 
-    // state = {counter:0}
+//     // constructor(props: AppProps){           // 2nd way to set inital state in TS(2)
+//     //     super(props)
+//     //     this.state = {counter: 0}
+//     // }
 
-    onIncrement = ():void =>{
-        this.setState({counter: this.state.counter+1})
+//     // state = {counter:0}
+
+//     onIncrement = ():void =>{
+//         this.setState({counter: this.state.counter+1})
     
-    }
+//     }
     
-    onDecrement = ():void =>{
-        this.setState({counter: this.state.counter - 1})
+//     onDecrement = ():void =>{
+//         this.setState({counter: this.state.counter - 1})
     
-    }
+//     }
 
-    render(){
-        return <div>
-            <button onClick={this.onIncrement}>INCREMENT</button>
-            <button onClick={this.onDecrement}>DECREMENT</button>
-            {this.state.counter}
-        </div>
-    }
-}
+//     render(){
+//         return <div>
+//             <button onClick={this.onIncrement}>INCREMENT</button>
+//             <button onClick={this.onDecrement}>DECREMENT</button>
+//             {this.state.counter}
+//         </div>
+//     }
+// }
+//---------------------------------------------------------------------------------------------------------------
+
+const store = createStore(reducers, applyMiddleware(thunk))
 
 ReactDOM.render(
-<App color= "red" />,
+<Provider store={store}>    
+<App/>
+</Provider>,
 document.querySelector('#root'))
 
---
+++ src/reducers/todos.ts
@@ -0,0 +1,13 @@
+import {Todo, FetchToDosAction} from '../actions'
+import { ActionTypes } from '../actions/types'
+
+export const todosReducer = (state:Todo[] = [], action: FetchToDosAction) =>{
+
+    switch(action.type){
+        case ActionTypes.fetchTodos:
+            return action.payload
+        default:
+            return state
+    }
+
+}
